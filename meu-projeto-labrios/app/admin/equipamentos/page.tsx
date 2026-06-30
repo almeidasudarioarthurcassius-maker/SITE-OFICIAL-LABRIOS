4a8b9ec3ea05381f1aed1035fb458beef280ae73
almeidasudarioarthurcassius-maker <almeidasudarioarthurcassius@gmail.com>
2026-06-30 16:22:33 -0400

    Update page.tsx

--- meu-projeto-labrios/app/admin/equipamentos/page.tsx
+++ meu-projeto-labrios/app/admin/equipamentos/page.tsx
@@ -1,4 +1,4 @@
-﻿import { supabase } from '../../../lib/supabase';
+﻿import { supabase } from '@/lib/supabase';
 import { revalidatePath } from 'next/cache';
 
 export const dynamic = 'force-dynamic';
